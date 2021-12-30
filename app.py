48a4dd4ae12b4dfb3ce258f28e162c5b1d5fa056
limeun6 <limeun6@naver.com>
2021-12-30 12:17:56 +0900

    임윤정/웹+머신러닝

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import urllib.request
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import sqlite3 as sql
 import numpy as np
+from sklearn.compose import ColumnTransformer
+#from ml.model import export_model
+#from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
@@ -68,18 +71,16 @@ def searchProducts():
 @app.route('/addrec/', methods=['POST', 'GET'])
 def addrec():
     if request.method == 'POST':
-        #입력값을 받는다.
+        #화면에서 입력받은값
         customer_lat = request.form['lat']
         customer_lng = request.form['lng']
         product_id = request.form['product_id']
 
+        #product_id로 검색
         con = sql.connect("database.db")
         # con.row_factory = sql.Row
-
         cur = con.cursor()
-        
         cur.execute("select * from OLIST_PRODUCTS a LEFT JOIN OLIST_SELLERS b on a.SELLER_ID = b.SELLER_ID where PRODUCT_ID = '" + product_id + "'")
-
         rows = cur.fetchone()
         product_id= rows[0]
         seller_id= rows[1]
@@ -91,17 +92,13 @@ def addrec():
         state= rows[8]
         lat= rows[9]
         lng= rows[10]
-
         
-        print(rows)
-        print(len(rows))
-        print("******************************"+lat + lng)
         #걸리는 시간을 구글에 검색
         origin          = customer_lat+","+customer_lng
         destination     = lat + "," + lng
         mode            = "driving"
         departure_time  = "now"
-        key='키값'
+        key='AIzaSyCoqfsNFyisaIOFPk_kTPTt4wUyMiaK3Rs'
 
         url = "https://maps.googleapis.com/maps/api/directions/json?origin="+ origin \
             + "&destination=" + destination \
@@ -119,18 +116,43 @@ def addrec():
 
         with open("./Agent_Transit_Directions.json","w") as rltStream :
             json.dump(responseJson,rltStream)
+        
         #검색 받은 값을 정리
         wholeDict = None
         with open("./Agent_Transit_Directions.json","r") as transitJson :
             wholeDict = dict(json.load(transitJson))
         print(wholeDict)
         path            = wholeDict["routes"][0]["legs"][0]
-        duration_sec    = path["distance"]["text"]
+        duration_km    = path["distance"]["text"]
+        #km삭제
+        duration_km=duration_km[0:len(duration_km)-2]
+        print("*******************"+duration_km)
+        #--------------------모델에서 값 추출---------------------
+        #입력값 정리
+        #연속형 컬럼 정규화
+        numerical_columns = [ price, freight_value, product_weight_g, duration_km, product_volume]
 
-        #----------------------------------------------
-        result=customer_lat+" " + customer_lng + " " + lat + " " + lng + ", 거리:" + duration_sec
-        print(result)
+        pipeline = ColumnTransformer([
+            ("Numerical", StandardScaler(), numerical_columns),
+            ], remainder = 'passthrough',)
+
+        train_data = pipeline.fit_transform(train_data)
+        test_data = pipeline.transform(test_data)
+
+        #우리가 필요한 행 정규화
+        last_row_list = test_no.iloc[-1,:].tolist()
+        last_row_df = pd.DataFrame([last_row_list],columns = test_no.columns.tolist())
+        last_row_data = pipeline.transform(last_row_df)
+
+        #모델 실행
+        prediction= model.predict(last_row_data)
+        label= prediction[0]
+        #결과출력
+        result=label
+        print("***************************"+result)
         return render_template('result.html', msg=result)
 
 if __name__ == '__main__':
+    #모델부르기
+    mode= joblib.load('mode.pkl')
     app.run(debug=True)
